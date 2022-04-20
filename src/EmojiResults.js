94876359a38fd8f28d6cb25e4fbafb8db70957b6
Ljubiša Knežević <munjainekoboj@gmail.com>
2022-04-20 09:58:13 +0200

    Added copy to clipboard

--- src/EmojiResults.js
+++ src/EmojiResults.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 function EmojiResults({emojis}) {
+
+  const copyToClipboard = (emoji) => {
+    // TODO: show a message when you've copied successfully
+    navigator.clipboard.writeText(emoji.char)
+  }
+
   return (
     <div className="emoji-results">
       {
         emojis.map(emoji => (
-          <div key={emoji.codes} className="emoji">
+          <div key={emoji.codes} className="emoji" onClick={() => copyToClipboard(emoji.char)}>
             {emoji.char} {emoji.name}
           </div>
         ))
